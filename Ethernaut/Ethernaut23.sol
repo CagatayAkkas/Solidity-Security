4e04cc5ee6bce7c22a7a613261f682f62f9ddb4a
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-04-19 12:21:45 +0300

    Update Ethernaut23.sol

--- Ethernaut/Ethernaut23.sol
+++ Ethernaut/Ethernaut23.sol
@@ -2,40 +2,19 @@ pragma solidity ^0.8.13;
 
 interface IDex {
     function token1() external view returns (address);
-
     function token2() external view returns (address);
-
     function swap(address from, address to, uint256 amount) external;
 }
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
-
     function balanceOf(address account) external view returns (uint256);
-
-    function transfer(
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint256);
-
+    function transfer(address recipient,uint256 amount) external returns (bool);
+    function allowance(address owner,address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
+    function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
+    event Approval(address indexed owner,address indexed spender,uint256 value
     );
 }
 
