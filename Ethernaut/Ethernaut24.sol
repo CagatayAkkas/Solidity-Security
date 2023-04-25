b6f09c66ed40674dbf85acb214e16d9e43ed9bbe
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-04-25 10:37:33 +0300

    Update Ethernaut24.sol

--- Ethernaut/Ethernaut24.sol
+++ Ethernaut/Ethernaut24.sol
@@ -2,21 +2,11 @@ pragma solidity ^0.8;
 
 interface IWallet {
     function admin() external view returns (address);
-
     function proposeNewAdmin(address _newAdmin) external;
-
     function addToWhitelist(address addr) external;
-
     function deposit() external payable;
-
     function multicall(bytes[] calldata data) external payable;
-
-    function execute(
-        address to,
-        uint256 value,
-        bytes calldata data
-    ) external payable;
-
+    function execute(address to,uint256 value,bytes calldata data) external payable;
     function setMaxBalance(uint256 _maxBalance) external;
 }
 
