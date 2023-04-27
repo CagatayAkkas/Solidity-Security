c2882676023c978a8e292f0f725665e88a4b916d
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-04-27 07:34:08 +0300

    Add files via upload

---
+++ Ethernaut/Ethernaut2.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8;
+
+interface IFallout {
+    function owner() external view returns (address);
+
+    // Sadece bu fonksiyonu çağırıyoruz =)
+    function Fal1out() external payable;
+}
